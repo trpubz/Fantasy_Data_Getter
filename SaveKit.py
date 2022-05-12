a8900c0dc33c580973f61060d2083f40cf68bd18
trpubz <taylor.pubins@me.com>
2022-05-11 22:27:19 -0700

    created SaveKit method

--- SaveKit.py
+++ SaveKit.py
@@ -1,2 +1,16 @@
 import os
 import glob
+
+
+@staticmethod
+def write_out(dir: str, fileName: str, ext: str, content: str):
+    # remove file at current directory
+    files = glob.glob(dir + "/*.html")
+    for removable in files:
+        if removable.__contains__(fileName+ext):
+            os.remove(removable)
+
+    # write out new contents
+    with open(dir+fileName+ext, 'w', encoding='utf-8') as f:
+        print(content, file=f)
+        f.close()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import selenium.webdriver.chromium.webdriver
 from selenium.webdriver.common.by import By
 import DriverKit
+import SaveKit
 
 
 def getESPNPlyrUniverse(url: str):
@@ -9,12 +10,22 @@ def getESPNPlyrUniverse(url: str):
     sdrvr.implicitly_wait(10)
     elmArticle = sdrvr.find_element(By.CSS_SELECTOR, 'div.article-body')
     rawHTML = elmArticle.get_attribute("innerHTML")
+    SaveKit.write_out(dir="/Users/Shared/Baseball HQ/", fileName='h2hPlayerList', ext=".html", content=rawHTML)
     sdrvr.close()
 
 
-# Press the green button in the gutter to run the script.
+def getFangraphsProjections():
+    pass
+
+
+def getSavantData():
+    pass
+
+
 if __name__ == '__main__':
     getESPNPlyrUniverse(url="https://www.espn.com/fantasy/baseball/story/_/id/33208450/fantasy-baseball-rankings-head"
                             "-head-category-rotiserrie-leagues-2022")
+    getFangraphsProjections()
+    getSavantData()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
