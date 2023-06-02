83e889cae48cd488580a3914d47e0b4391657921
trpubz <taylor.pubins@me.com>
2023-06-02 15:08:18 -0700

    added TODO for BREFID addition to player object, would result in refactor of Fangraphs_Data_Getter

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ import pandas as pd
 rawHTML: str = ""
 players: list[Player] = []
 
+
 def getESPNPlyrUniverse(url: str):
     """
     Function that navigates to the league's player rater URL.
@@ -30,8 +31,8 @@ def getESPNPlyrUniverse(url: str):
     sdrvr = DKDriverConfig(dirDownload=dirHQ, headless=True)
     sdrvr.get(url)
     sdrvr.implicitly_wait(10)
-    
-    global rawHTML # bring in global variable
+
+    global rawHTML  # bring in global variable
 
     # get the position radio buttons
     pickerGroup = sdrvr.find_element(By.CSS_SELECTOR, "#filterSlotIds")
@@ -42,7 +43,7 @@ def getESPNPlyrUniverse(url: str):
     for button in position_buttons:
         posGroup = button.text
         if posGroup == "Batters" or posGroup == "Pitchers":
-            try: 
+            try:
                 button.click()
                 # give the page time to load
                 sleep(5)
@@ -51,21 +52,22 @@ def getESPNPlyrUniverse(url: str):
                 # TODO: this will override the number of pages hardcoding
                 # adjust number of pages depending on position
                 num_pages = 11 if posGroup == "Batters" else 14
-                
+
                 for page in range(1, num_pages + 1):
                     sleep(5)
                     # get the HTML of the page and parse it with BeautifulSoup
                     soup = BeautifulSoup(sdrvr.page_source, 'lxml')
                     # find the tables
-                    tables = soup.find_all('table')[:2] # there are 2 tables on the page, the first is the player names and the second is the player rater data
+                    tables = soup.find_all('table')[
+                             :2]  # there are 2 tables on the page, the first is the player names and the second is the player rater data
                     # print(tables)
-                    
+
                     # there are 2 header rows, shed the first one
                     playerInfoColumns = tables[0].find_all('tr')[1:]
                     playerRaterColumns = tables[1].find_all('tr')[1:]
                     # add the same rows from each table to the combined table
                     for i in range(0, len(playerInfoColumns)):
-                        if i == 0 and page == 1 and posGroup == "Batters": # only add the headers once
+                        if i == 0 and page == 1 and posGroup == "Batters":  # only add the headers once
                             headers = playerInfoColumns[i].contents + playerRaterColumns[i].contents
                             combinedHeaderRow = BeautifulSoup('', 'lxml').new_tag('thead')
                             for header in headers:
@@ -79,7 +81,8 @@ def getESPNPlyrUniverse(url: str):
                                 combinedPlayerRow.append(plyr)
                             # print(wholeRow)
                             combinedTable.append(combinedPlayerRow)
-                        else: continue # if i == 0 on any other page, then skip it
+                        else:
+                            continue  # if i == 0 on any other page, then skip it
                     # go to the next page
                     print(f"Finished processing page {page} of {num_pages} for {posGroup}")
                     next_button = sdrvr.find_element(By.CSS_SELECTOR, "button.Button.Pagination__Button--next")
@@ -95,6 +98,7 @@ def getESPNPlyrUniverse(url: str):
     # write out the combined table to a file
     IOKit.writeOut(fileName="tempESPNPlayerUniverse", ext=".html", content=rawHTML)
     sdrvr.close()
+
 
 def fetchPlayerKeyMap(url) -> pd.DataFrame:
     # check to see if tempPlayerKeyMap.json exists before proceeding
@@ -113,30 +117,33 @@ def fetchPlayerKeyMap(url) -> pd.DataFrame:
         print(f"{filename} exists, returning dataframe")
         return df
     else:
-        df = pd.read_html(url, header=1, index_col=None)[0] # returns a list of dataframes, so we need to index the first one, header is set to the second row
-        df.drop(columns=df.columns[0], inplace=True) # drop the first column, which is the html index column, this uses pandas indexing
-        df.drop(index=0, inplace=True) # drop the first row, which is NaN
-        print(f"{df.count} number of players before dropping empty values") 
+        df = pd.read_html(url, header=1, index_col=None)[
+            0]  # returns a list of dataframes, so we need to index the first one, header is set to the second row
+        df.drop(columns=df.columns[0],
+                inplace=True)  # drop the first column, which is the html index column, this uses pandas indexing
+        df.drop(index=0, inplace=True)  # drop the first row, which is NaN
+        print(f"{df.count} number of players before dropping empty values")
         df.dropna(subset=["MLBID", "ESPNID"], inplace=True)
         print(f"{df.count} number of players after dropping empty values")
         # drop all rows that have NaN in the ESPNID column
-        df = df.astype({"ESPNID": int, "MLBID": int}) # casting from float to str requires int as an intermediate step
+        df = df.astype({"ESPNID": int, "MLBID": int})  # casting from float to str requires int as an intermediate step
         df = df.astype({"ESPNID": str, "MLBID": str})
         df.to_json(filename, orient="records")
         print(df)
         print("File pulled from the interwebs, returning dataframe")
         return df
 
+
 def buildPlayerUniverse(dfKeyMap: pd.DataFrame):
     """
     Function that parses the ESPN Fantasy Universe HTML file and extracts the player names and their ESPN Player IDs.
     This function is dependent on the ESPN Fantasy Universe HTML file being downloaded and saved to the project root.
-    :param df: pandas dataframe that contains the player key map
+    :param dfKeyMap: pandas dataframe that contains the player key map
     :return: none
     """
     os.environ['PYDEVD_WARN_SLOW_RESOLVE_TIMEOUT'] = '1.5s'
 
-    global rawHTML # global keyword allows access to the global variable
+    global rawHTML  # global keyword allows access to the global variable
     if rawHTML == "":
         rawHTML = IOKit.readIn(fileName='tempESPNPlayerUniverse', ext=".html")
 
@@ -146,13 +153,15 @@ def buildPlayerUniverse(dfKeyMap: pd.DataFrame):
 
     playerRows = soup.find_all("tr")
 
-    for player in playerRows: # first row is designated by the "th" tag, so no need to skip it here
+    for player in playerRows:  # first row is designated by the "th" tag, so no need to skip it here
         playerData = player.find_all("td")
-        idLoc: str = playerData[1].find(class_ = "player-headshot").find("img").get("src")
+        idLoc: str = playerData[1].find(class_="player-headshot").find("img").get("src")
         espnID = re.findall(r'full/(\d+)\.png', idLoc)[0]
         if espnID is None: print(f"espnID is None for {playerData[1]}")
         # shohei player id is 39382
         try:
+            # TODO: add BREFID to the player object if present in the key map; may need to refactor the
+            #  Fangraphs_Data_Getter
             fangraphsID = dfKeyMap[dfKeyMap["ESPNID"] == espnID]["FANGRAPHSID"].values[0]
             savantID = dfKeyMap[dfKeyMap["ESPNID"] == espnID]["MLBID"].values[0]
             players.append(Player().from_data(playerData, espnID, fangraphsID, savantID))
@@ -165,6 +174,7 @@ def buildPlayerUniverse(dfKeyMap: pd.DataFrame):
 
     # save the player universe to a json file
     IOKit.writeOut(dir=dirHQ, fileName="espnPlayerUniverse", ext=".json", content=players)
+
 
 def deleteTempFiles():
     """
@@ -189,4 +199,3 @@ if __name__ == '__main__':
     buildPlayerUniverse(dfKeyMap=dfKeyMap)
 
     # pipdeleteTempFiles()
-
